46de4859a962fb50dbb28a5f9bb61647c97712c8
VaibhavApraj <vaibhavapraj702@gmail.com>
2023-04-21 14:49:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstProject';
+  title:any = 'firstProject';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.title);
+  }
+
+
 }
